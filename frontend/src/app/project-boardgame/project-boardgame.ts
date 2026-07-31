80bebe9743473d54c2bcf52602d261f531c238e4
Anna <annaakosua.stepper@gmail.com>
2026-08-01 01:10:57 +0200

    feat:polshing projekt page

--- frontend/src/app/project-boardgame/project-boardgame.ts
+++ frontend/src/app/project-boardgame/project-boardgame.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Nav } from "../nav/nav";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-project-boardgame',
-  imports: [Nav],
+  imports: [Nav, Footer],
   templateUrl: './project-boardgame.html',
   styleUrl: './project-boardgame.css',
 })
